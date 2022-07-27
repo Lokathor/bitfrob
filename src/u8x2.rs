b64673969c2fea00a2a52b4b95c848ab7ed8b90b
Lokathor <zefria@gmail.com>
2022-07-27 10:04:46 -0600

    Update u8x2.rs

--- src/u8x2.rs
+++ src/u8x2.rs
@@ -1,1 +1,27 @@
+use crate::*;
 
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct u8x2(u16);
+impl u8x2 {
+  #[inlie]
+  #[must_use]
+  pub const fn low(self) -> u8 {
+    u16_get_value::<0,7>(self.0) as u8
+  }
+  #[inline]
+  #[must_use]
+  pub const fn with_low(self, low: u8) -> Self {
+    Self(u16_with_value::<0,7>(self.0,u16::from(low)))
+  }
+  #[inlie]
+  #[must_use]
+  pub const fn high(self) -> u8 {
+    u16_get_value::<8,15>(self.0) as u8
+  }
+  #[inline]
+  #[must_use]
+  pub const fn with_high(self, high: u8) -> Self {
+    Self(u16_with_value::<8,15>(self.0,u16::from(high)))
+  }
+}
