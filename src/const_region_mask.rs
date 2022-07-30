82a0621d44de0a8b032de9c29e94dba7ac712afb
Lokathor <zefria@gmail.com>
2022-07-30 10:19:47 -0600

    updates

--- src/const_region_mask.rs
+++ src/const_region_mask.rs
@@ -1,7 +1,7 @@
 /// Generates a bit mask where all bits in the region are 1.
 ///
-/// The `low` and `high` values form an *inclusive* bit range where the mask bits
-/// are 1.
+/// The `low` and `high` values form an *inclusive* bit range where the mask
+/// bits are 1.
 ///
 /// ```
 /// # use bitfrob::*;
@@ -13,6 +13,8 @@
 /// ## Panics
 /// * Both `low` and `high` must be less than 32.
 /// * `low` must be less than `high`.
+#[inline]
+#[must_use]
 pub const fn u32_region_mask(low: u32, high: u32) -> u32 {
   assert!(low < u32::BITS);
   assert!(high < u32::BITS);
@@ -20,10 +22,22 @@ pub const fn u32_region_mask(low: u32, high: u32) -> u32 {
   (((-1_i32) as u32) >> ((u32::BITS - 1) - (high - low))) << low
 }
 
+/// Like [`u32_region_mask`], but forces the value into an associated constant.
+///
+/// * Advantage: the compiler is forced to compute the constant at compile time,
+///   regardless of debug level.
+/// * Disadvantage: the `L` and `H` must themselves be constants.
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct U32ConstRegionMask<const L: u32, const H: u32>;
+impl<const L: u32, const H: u32> U32ConstRegionMask<L, H> {
+  /// The computed bit mask.
+  pub const OUT: u32 = u32_region_mask(L, H);
+}
+
 /// Generates a bit mask where all bits in the region are 1.
 ///
-/// The `low` and `high` values form an *inclusive* bit range where the mask bits
-/// are 1.
+/// The `low` and `high` values form an *inclusive* bit range where the mask
+/// bits are 1.
 ///
 /// ```
 /// # use bitfrob::*;
@@ -35,6 +49,8 @@ pub const fn u32_region_mask(low: u32, high: u32) -> u32 {
 /// ## Panics
 /// * Both `low` and `high` must be less than 16.
 /// * `low` must be less than `high`.
+#[inline]
+#[must_use]
 pub const fn u16_region_mask(low: u32, high: u32) -> u16 {
   assert!(low < u16::BITS);
   assert!(high < u16::BITS);
@@ -42,10 +58,22 @@ pub const fn u16_region_mask(low: u32, high: u32) -> u16 {
   (((-1_i16) as u16) >> ((u16::BITS - 1) - (high - low))) << low
 }
 
+/// Like [`u16_region_mask`], but forces the value into an associated constant.
+///
+/// * Advantage: the compiler is forced to compute the constant at compile time,
+///   regardless of debug level.
+/// * Disadvantage: the `L` and `H` must themselves be constants.
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct U16ConstRegionMask<const L: u32, const H: u32>;
+impl<const L: u32, const H: u32> U16ConstRegionMask<L, H> {
+  /// The computed bit mask.
+  pub const OUT: u16 = u16_region_mask(L, H);
+}
+
 /// Generates a bit mask where all bits in the region are 1.
 ///
-/// The `low` and `high` values form an *inclusive* bit range where the mask bits
-/// are 1.
+/// The `low` and `high` values form an *inclusive* bit range where the mask
+/// bits are 1.
 ///
 /// ```
 /// # use bitfrob::*;
@@ -57,9 +85,23 @@ pub const fn u16_region_mask(low: u32, high: u32) -> u16 {
 /// ## Panics
 /// * Both `low` and `high` must be less than 32.
 /// * `low` must be less than `high`.
-pub const fn u32_region_mask(low: u32, high: u32) -> u8 {
+#[inline]
+#[must_use]
+pub const fn u8_region_mask(low: u32, high: u32) -> u8 {
   assert!(low < u8::BITS);
   assert!(high < u8::BITS);
   assert!(low < high);
   (((-1_i8) as u8) >> ((u8::BITS - 1) - (high - low))) << low
+}
+
+/// Like [`u8_region_mask`], but forces the value into an associated constant.
+///
+/// * Advantage: the compiler is forced to compute the constant at compile time,
+///   regardless of debug level.
+/// * Disadvantage: the `L` and `H` must themselves be constants.
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct U8ConstRegionMask<const L: u32, const H: u32>;
+impl<const L: u32, const H: u32> U8ConstRegionMask<L, H> {
+  /// The computed bit mask.
+  pub const OUT: u8 = u8_region_mask(L, H);
 }
--- src/fns.rs
+++ src/fns.rs
@@ -1,68 +1,4 @@
-/// Generates a bit mask where all bits in the region are 1.
-///
-/// The `L` and `H` parameters form an *inclusive* bit range where the mask bits
-/// are 1.
-///
-/// ```
-/// # use bitfrob::*;
-/// assert_eq!(u32_region_mask::<0, 2>(), 0b0000_0111_u32);
-/// assert_eq!(u32_region_mask::<1, 3>(), 0b0000_1110_u32);
-/// assert_eq!(u32_region_mask::<4, 7>(), 0b1111_0000_u32);
-/// ```
-///
-/// ## Panics
-/// * Both `L` and `H` must be less than 32.
-/// * `L` must be less than `H`.
-pub const fn u32_region_mask<const L: u32, const H: u32>() -> u32 {
-  assert!(L < u32::BITS);
-  assert!(H < u32::BITS);
-  assert!(L < H);
-  (((-1_i32) as u32) >> ((u32::BITS - 1) - (H - L))) << L
-}
-
-/// Generates a bit mask where all bits in the region are 1.
-///
-/// The `L` and `H` parameters form an *inclusive* bit range where the mask bits
-/// are 1.
-///
-/// ```
-/// # use bitfrob::*;
-/// assert_eq!(u16_region_mask::<0, 2>(), 0b0000_0111_u16);
-/// assert_eq!(u16_region_mask::<1, 3>(), 0b0000_1110_u16);
-/// assert_eq!(u16_region_mask::<4, 7>(), 0b1111_0000_u16);
-/// ```
-///
-/// ## Panics
-/// * Both `L` and `H` must be less than 16.
-/// * `L` must be less than `H`.
-pub const fn u16_region_mask<const L: u32, const H: u32>() -> u16 {
-  assert!(L < u16::BITS);
-  assert!(H < u16::BITS);
-  assert!(L < H);
-  (((-1_i16) as u16) >> ((u16::BITS - 1) - (H - L))) << L
-}
-
-/// Generates a bit mask where all bits in the region are 1.
-///
-/// The `L` and `H` parameters form an *inclusive* bit range where the mask bits
-/// are 1.
-///
-/// ```
-/// # use bitfrob::*;
-/// assert_eq!(u8_region_mask::<0, 2>(), 0b0000_0111_u8);
-/// assert_eq!(u8_region_mask::<1, 3>(), 0b0000_1110_u8);
-/// assert_eq!(u8_region_mask::<4, 7>(), 0b1111_0000_u8);
-/// ```
-///
-/// ## Panics
-/// * Both `L` and `H` must be less than 8.
-/// * `L` must be less than `H`.
-pub const fn u8_region_mask<const L: u32, const H: u32>() -> u8 {
-  assert!(L < u8::BITS);
-  assert!(H < u8::BITS);
-  assert!(L < H);
-  (((-1_i8) as u8) >> ((u8::BITS - 1) - (H - L))) << L
-}
+use crate::*;
 
 /// Gets the `B` bit.
 ///
@@ -96,10 +32,7 @@ pub const fn u32_with_bit<const B: u32>(x: u32, val: bool) -> u32 {
 #[inline]
 #[must_use]
 pub const fn u32_get_region<const L: u32, const H: u32>(x: u32) -> u32 {
-  assert!(L < 32);
-  assert!(H < 32);
-  assert!(L < H);
-  let mask = u32_region_mask::<L, H>();
+  let mask = U32ConstRegionMask::<L, H>::OUT;
   x & mask
 }
 
@@ -113,10 +46,7 @@ pub const fn u32_get_region<const L: u32, const H: u32>(x: u32) -> u32 {
 pub const fn u32_with_region<const L: u32, const H: u32>(
   x: u32, val: u32,
 ) -> u32 {
-  assert!(L < 32);
-  assert!(H < 32);
-  assert!(L < H);
-  let mask = u32_region_mask::<L, H>();
+  let mask = U32ConstRegionMask::<L, H>::OUT;
   (x & !mask) | val
 }
 
@@ -174,10 +104,7 @@ pub const fn u16_with_bit<const B: u32>(x: u16, val: bool) -> u16 {
 #[inline]
 #[must_use]
 pub const fn u16_get_region<const L: u32, const H: u32>(x: u16) -> u16 {
-  assert!(L < 16);
-  assert!(H < 16);
-  assert!(L < H);
-  let mask = u16_region_mask::<L, H>();
+  let mask = U16ConstRegionMask::<L, H>::OUT;
   x & mask
 }
 
@@ -191,10 +118,7 @@ pub const fn u16_get_region<const L: u32, const H: u32>(x: u16) -> u16 {
 pub const fn u16_with_region<const L: u32, const H: u32>(
   x: u16, val: u16,
 ) -> u16 {
-  assert!(L < 16);
-  assert!(H < 16);
-  assert!(L < H);
-  let mask = u16_region_mask::<L, H>();
+  let mask = U16ConstRegionMask::<L, H>::OUT;
   (x & !mask) | val
 }
 
@@ -272,10 +196,7 @@ pub const fn u8_with_value<const L: u32, const H: u32>(x: u8, val: u8) -> u8 {
 #[inline]
 #[must_use]
 pub const fn u8_get_region<const L: u32, const H: u32>(x: u8) -> u8 {
-  assert!(L < 8);
-  assert!(H < 8);
-  assert!(L < H);
-  let mask = u8_region_mask::<L, H>();
+  let mask = U8ConstRegionMask::<L, H>::OUT;
   x & mask
 }
 
@@ -287,9 +208,6 @@ pub const fn u8_get_region<const L: u32, const H: u32>(x: u8) -> u8 {
 #[inline]
 #[must_use]
 pub const fn u8_with_region<const L: u32, const H: u32>(x: u8, val: u8) -> u8 {
-  assert!(L < 8);
-  assert!(H < 8);
-  assert!(L < H);
-  let mask = u8_region_mask::<L, H>();
+  let mask = U8ConstRegionMask::<L, H>::OUT;
   (x & !mask) | val
 }
