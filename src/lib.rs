56f730e105c1ae78102c53212c8a930748be9c47
Lokathor <zefria@gmail.com>
2022-07-30 10:23:22 -0600

    shuffle around how the region mask stuff works (#5)
    * Create const_region_mask.rs
    
    * fixes the u8x2 visibility bug
    
    * updates
    
    * fix doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,21 @@
 #![no_std]
 #![forbid(unsafe_code)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(clippy::missing_const_for_fn)]
+#![warn(clippy::missing_inline_in_public_items)]
+#![warn(clippy::missing_panics_doc)]
+#![warn(clippy::must_use_candidate)]
+
+//! A crate to help with bit manipulation of integers.
+
+mod const_region_mask;
+pub use const_region_mask::*;
 
 mod fns;
 pub use fns::*;
 
-mod u8x2;
-pub use u8x2::*;
+// Note: module name must not match type name!
+mod u8x2_;
+pub use u8x2_::*;
