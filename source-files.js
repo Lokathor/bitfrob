fed70ed02fc61ab43da7832f23e3ca314ed1ac29
Lokathor <zefria@gmail.com>
2022-08-06 02:37:18 +0000

    Deploying to gh-pages from @ Lokathor/bitfrob@ae8182d841043608487d43727e0eadfae5f0769e 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"bitfrob":["",[],["get_bit.rs","get_region.rs","get_value.rs","lib.rs","region_mask.rs","u8x2_.rs","with_bit.rs","with_region.rs","with_value.rs"]]\
+"bitfrob":["",[],["bit_iter.rs","get_bit.rs","get_region.rs","get_value.rs","lib.rs","region_mask.rs","replicate_bits.rs","u8x2_.rs","with_bit.rs","with_region.rs","with_value.rs"]]\
 }');
 createSourceSidebar();
